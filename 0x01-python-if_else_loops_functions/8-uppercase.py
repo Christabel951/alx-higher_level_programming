fcc366a411ffec23d78d453d4939c6d246459da2
Christabel951 <crkomo@gmail.com>
2021-09-08 12:53:20 +0000

    Replace char_with_ASCII

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def uppercase(str):
     for i in str:
-        if i >= 'a' and i <= 'z':
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
             upper = chr(ord(i) - 32)
         else:
             upper = i
