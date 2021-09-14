1e8a261de9db3df4c22b068be93c6cc6943e69a6
Christabel951 <crkomo@gmail.com>
2021-09-14 02:51:53 +0000

    add file 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = my_list[:]
+    for i in new_list:
+        if i % 2 == 0:
+            i = True
+        else:
+            i = False
+    return new_list
