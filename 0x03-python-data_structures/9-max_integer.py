09b6ac871d33b11a1bf8b42e57c3068512a39527
Christabel951 <crkomo@gmail.com>
2021-09-14 04:24:59 +0000

    change upper loop bound value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -9,8 +9,8 @@ def max_integer(my_list=[]):
             if len(my_list) == 1:
                 return my_list[i]
             else:
-                if my_list[i] > my_list[i + 1]:
-                    if bigger < my_list[i]:
-                        bigger = my_list[i]
+                if my_list[i] < my_list[i + 1]:
+                    if bigger < my_list[i + 1]:
+                        bigger = my_list[i + 1]
                 i += 1
         return bigger
