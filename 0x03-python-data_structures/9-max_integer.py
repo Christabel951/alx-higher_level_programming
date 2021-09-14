76d25e0d46f865aff8517d64905ae88e6fe40e5b
Christabel951 <crkomo@gmail.com>
2021-09-14 05:06:33 +0000

    Change if_max_swap_value conditions

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,14 +3,11 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        bigger = 0
-        i = 0
-        while i < len(my_list) - 1:
-            if len(my_list) == 1:
-                return my_list[i]
-            else:
-                if my_list[i] < my_list[i + 1]:
-                    if bigger < my_list[i + 1]:
-                        bigger = my_list[i + 1]
-                i += 1
+        bigger = my_list[0]
+        i = 1
+        while i < len(my_list):
+            if len(my_list) > 1:
+                if bigger < my_list[i]:
+                    bigger = my_list[i]
+            i += 1
         return bigger
