aae9d443c606c2e23898bd82c498c19f2d8e2204
Christabel951 <crkomo@gmail.com>
2021-09-29 08:19:20 +0000

    add file 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" No imported module """
+
+
+class Square:
+    """ Represent a square """
+    def __init__(self, size):
+        """ Initialize class with size value using __init__ method
+
+            Args:
+                size(no type/value verification): pass value to a private
+                            instance attribute.
+        """
+        self.__size = size
