945b84e32548418a95cda98977e63764119cc8d3
Christabel951 <crkomo@gmail.com>
2021-10-11 21:48:17 +0000

    add file 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" No imported module """
+
+
+class BaseGeometry:
+    """ Represents an empty class """
+    pass
+
+    def area(self):
+        """
+        Raises an Exception with the message
+        'area() is not implemented'.
+
+        """
+
+        raise Exception('area() is not implemented')
