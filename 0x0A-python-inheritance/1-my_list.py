0b389a3d7711a580b5749c4841c4c5f5a961b04f
Christabel951 <crkomo@gmail.com>
2021-10-13 16:08:50 +0000

    rectify indentation error

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
-""" 
-No imported module
-Module 1-my_list - MyList class inherits from the list class
+""" Module 1-my_list
+Creates MyList class which inherits from the list class
 
 """
 
+
 class MyList(list):
+    """ Class MyList inherits from list - subclass """
 
     def print_sorted(self):
         """ Prints list items sorted in ascending order """
-    print(sorted(self))
-        
-        
+        print(sorted(self))
