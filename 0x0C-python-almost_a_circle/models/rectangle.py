daa3580e53288d1301abacc1052f675589eacda9
Christabel951 <crkomo@gmail.com>
2021-10-18 20:36:43 +0000

    remove whitespace

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,64 @@
+""" Module rectangle
+Inherits from Base Class
+
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Represents a rectangle object and inherits from Base class """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Create a rectangle object
+        Args:
+            __width: width value
+            __height: height value
+            __x: x value
+            __y: y value
+            id : id
+
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """ Retrieve width attribute value """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Set the value of width """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Retrieve the height value """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Set height value """
+        self.__height = value
+
+    @property
+    def x(self):
+        """Retrieve the value of x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Set the value of x """
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Retrieve the value of y """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Set value of y """
+        self.__y = value
